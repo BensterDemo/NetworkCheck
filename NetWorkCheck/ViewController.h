dc68692e9c29f0b24feff4bb671a4ba9008ad730
Benster <benster.shen@31huiyi.com>
2015-04-20 09:13:27 +0800

    Initial Commit

---
+++ NetWorkCheck/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NetWorkCheck
+//
+//  Created by Benster on 15/4/20.
+//  Copyright (c) 2015年 Benster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
